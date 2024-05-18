7a047bafe471fd8a7277cc084038cf48fcf14193
Leoyalta <149539157+Leoyalta@users.noreply.github.com>
2024-05-18 10:26:22 +0200

    HW-10.js

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,45 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input-delay');
+let delay;
+
+input.addEventListener('input', event=> {
+  delay = event.currentTarget.value;
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  function createPromise(delay, state) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (state === 'fulfilled') {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    });
+  }
+
+  createPromise(delay, form.elements.state.value)
+    .then(delay => {
+      iziToast.show({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        backgroundColor: '#59A10D',
+        messageColor: '#FFFFFF',
+      });
+    })
+    .catch(delay => {
+      iziToast.show({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        backgroundColor: '#EF4040',
+        messageColor: '#FFFFFF',
+      });
+    });
+  form.reset();
+});
